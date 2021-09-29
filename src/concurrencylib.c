381f10e8ef44c5eef02495ad8bf0ab90b3c466a9
Joseph Terribile <kartingman25@gmail.com>
2021-09-29 15:42:52 -0400

    extroplate out thread impl to own c file

---
+++ src/concurrencylib.c
@@ -0,0 +1,35 @@
+#include "concurrencylib.h"
+
+CSThread* createThread(threadFunc func, void* arg) {
+    CSThread* thread = malloc(sizeof(CSThread));
+    if (thread == NULL) {
+        return NULL;
+    }
+
+    #if defined(_WIN32) // windows
+    thread->thread = CreateThread(0, 0, func, arg, 0, 0);
+    #elif defined(__APPLE__) || defined(__linux__)
+    pthread_create(&thread->thread, NULL, func, arg);
+    #endif
+
+    return thread;
+}
+
+int joinThread(CSThread* thread) {
+    #if defined(_WIN32) // windows
+    WaitForSingleObject(thread->thread, INFINITE);
+    GetExitCodeThread(thread->thread, &((DWORD) thread->returnVal));
+    #elif defined(__APPLE__) || defined(__linux__)
+    pthread_join(thread->thread, &thread->returnVal);
+    #endif
+    // return 0 if error, this is a TODO
+    return 1;
+}
+
+void freeCSThread(CSThread* thread) {
+    // make sure it is joined
+    // TODO
+
+    // free struct
+    free(thread);
+}
--- src/concurrencylib.h
+++ src/concurrencylib.h
@@ -32,34 +32,8 @@ typedef struct CSThread {
 } CSThread;
 
 
-CSThread* createThread(threadFunc func, void* arg) {
-    CSThread* thread = malloc(sizeof(CSThread));
-    if (thread == NULL) {
-        return NULL;
-    }
+CSThread* createThread(threadFunc func, void* arg);
 
-    #if defined(_WIN32) // windows
-    thread->thread = CreateThread(0, 0, func, arg, 0, 0);
-    #elif defined(__APPLE__) || defined(__linux__)
-    pthread_create(&thread->thread, NULL, func, arg);
-    #endif
+int joinThread(CSThread* thread);
 
-    return thread;
-}
-
-int joinThread(CSThread* thread) {
-    #if defined(_WIN32) // windows
-    WaitForSingleObject(thread->thread, INFINITE);
-    GetExitCodeThread(thread->thread, &((DWORD) thread->returnVal));
-    #elif defined(__APPLE__) || defined(__linux__)
-    pthread_join(thread->thread, &thread->returnVal);
-    #endif
-}
-
-void freeCSThread(CSThread* thread) {
-    // make sure it is joined
-    // TODO
-
-    // free struct
-    free(thread);
-}
+void freeCSThread(CSThread* thread);

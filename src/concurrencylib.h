6b2f507faeb1e9d9aa7ccac4a279215c97691c79
Joseph Terribile <kartingman25@gmail.com>
2021-09-29 15:35:08 -0400

    crossplatform threads go brrrrrr

--- src/concurrencylib.h
+++ src/concurrencylib.h
@@ -1,4 +1,64 @@
 #include <stdio.h>
 
+#ifdef _WIN32 // windows
+#include <windows.h>
+typedef DWORD WINAPI (*threadFunc) (LPVOID param);
+#define THREAD_RET DWORD WINAPI
+#define THREAD_PARAM LPVOID
+#elif __linux__ // linux stuff
+#include <pthread.h>
+typedef void* (*threadFunc) (void* param);
+#define THREAD_RET void*
+#define THREAD_PARAM void*
+#elif __APPLE__
+#include <pthread.h>
+typedef void* (*threadFunc) (void* param);
+#define THREAD_RET void*
+#define THREAD_PARAM void*
+#endif
+
 // meta: proto type for the compiled name of the user function so we have access to it in our program
 int userMain(void);
+
+// concurrency simulator thread
+typedef struct CSThread {
+    #if defined(_WIN32) // windows
+    HANDLE thread;
+    #elif defined(__APPLE__) || defined(__linux__)
+    pthread_t thread;
+    #endif
+    void* returnVal;
+} CSThread;
+
+
+CSThread* createThread(threadFunc func, void* arg) {
+    CSThread* thread = malloc(sizeof(CSThread));
+    if (thread == NULL) {
+        return NULL;
+    }
+
+    #if defined(_WIN32) // windows
+    thread->thread = CreateThread(0, 0, func, arg, 0, 0);
+    #elif defined(__APPLE__) || defined(__linux__)
+    pthread_create(&thread->thread, NULL, func, arg);
+    #endif
+
+    return thread;
+}
+
+int joinThread(CSThread* thread) {
+    #if defined(_WIN32) // windows
+    WaitForSingleObject(thread->thread, INFINITE);
+    GetExitCodeThread(thread->thread, &((DWORD) thread->returnVal));
+    #elif defined(__APPLE__) || defined(__linux__)
+    pthread_join(thread->thread, &thread->returnVal);
+    #endif
+}
+
+void freeCSThread(CSThread* thread) {
+    // make sure it is joined
+    // TODO
+
+    // free struct
+    free(thread);
+}
--- src/ourprogram.c
+++ src/ourprogram.c
@@ -1,10 +1,44 @@
 #include "concurrencylib.h"
 
+THREAD_RET thing1(THREAD_PARAM param) {
+    int threadNum = (int) param;
+    printf("Inside Thread %d, doing thing 1\n", threadNum);
+
+    return (void*) 123;
+}
+
+THREAD_RET thing2(THREAD_PARAM param) {
+    int threadNum = (int) param;
+    printf("Inside Thread %d, doing thing 2\n", threadNum);
+
+    return (void*) "abc";
+}
+
 int main(int argc, char** argv) {
+    // show overwriting of user program
     printf("Program Start: %s\n", __func__);
     printf("Calling userMain...\n");
     userMain();
     printf("Back in %s\n", __func__);
-    printf("Exiting\n");
+
+    // create threads
+    CSThread* thread1 = createThread(thing1, (void*) 1);
+    CSThread* thread2 = createThread(thing2, (void*) 2);
+
+    // wait for threads to finish and join them back into this thread
+    joinThread(thread1);
+    joinThread(thread2);
+
+    // get return values for printing
+    int retVal1 = (int) thread1->returnVal;
+    char* retVal2 = (char*) thread2->returnVal;
+
+    printf("Thread 1 returns: %d\n", retVal1);
+    printf("Thread 2 returns: %s\n", retVal2);
+
+    // free malloced CSThread struct
+    freeCSThread(thread1);
+    freeCSThread(thread2);
+
     return 0;
 }
